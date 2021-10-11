3c4bf909936bae1c9e01e9b00056d9a60b817360
hello_world <haileformal@gmail.com>
2021-10-11 19:46:00 +0300

    design UI for login window

--- src/main/java/com/nodamin/emailapp/controller/LoginWindowController.java
+++ src/main/java/com/nodamin/emailapp/controller/LoginWindowController.java
@@ -3,10 +3,19 @@ package com.nodamin.emailapp.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 public class LoginWindowController extends BaseWindowController {
+    // ids from the fxml file
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordTextField;
 
     // constructor
     public LoginWindowController(String fxmlName, Stage stage) {
